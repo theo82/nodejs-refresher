37aaf3c7be99bd502037838ad9bae7ec3fe6b081
theo82 <theoaristi53@gmail.com>
2020-10-22 09:55:35 +0300

    Creating a Simple Backend &

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MognoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://theo:<password>@cluster0.1o6wa.mongodb.net/products_test?retryWrites=true&w=majority';
+
+const createProduct = async (req, res, next) => {
+
+};
+
+const getProducts = async (req, res, next) => {
+
+}
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
