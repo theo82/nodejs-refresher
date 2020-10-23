cf438bce73a3ab413d9c15d9e4238fcbe2d4ad1f
theo82 <theoaristi53@gmail.com>
2020-10-23 09:23:26 +0300

    Getting Data from the Database

--- mongo.js
+++ mongo.js
@@ -1,14 +1,43 @@
-const MognoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb+srv://theo:<password>@cluster0.1o6wa.mongodb.net/products_test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://theo:TVTHa5RC4BsR6iz@cluster0.1o6wa.mongodb.net/products_test?retryWrites=true&w=majority';
 
 const createProduct = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    }
 
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const products = db.collection('products').insertOne(newProduct);
+    } catch (error) {
+        return res.json({message: 'Could not store data.'});
+    };
+    client.close();
+
+    res.json(newProduct);
 };
 
 const getProducts = async (req, res, next) => {
 
-}
+    const client = new MongoClient(url);
+
+    let products;
+    try{
+    await client.connect();
+    const db = client.db();
+     products = await db.collection('products').find().toArray();
+    } catch(error) {
+        return res.json({ message: 'Could not retrieve products. '});
+    }
+    client.close();
+
+    res.json(products);
+};
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
